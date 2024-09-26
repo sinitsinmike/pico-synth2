7101b1fa12abf30da08847a42fc3ee77807c1f58
sinitsinmike <sinitsinmike@yahoo.com>
2024-09-26 20:55:50 +0300

    Update driver-eeprom.c

--- firmware/tui/driver-eeprom.c
+++ firmware/tui/driver-eeprom.c
@@ -25,9 +25,9 @@ eeprom_init(ps_tui_t *tui)
     tui->eeprom._page_size = 0;
 
     switch (tui->eeprom.ic) {
-        case PS_TUI_EEPROM_24LC256:
-            tui->eeprom._page_size = 64;
-            break;
+    //    case PS_TUI_EEPROM_24LC256:
+      //      tui->eeprom._page_size = 64;
+        //    break;
 
         case PS_TUI_EEPROM_24LC512:
             tui->eeprom._page_size = 128;
